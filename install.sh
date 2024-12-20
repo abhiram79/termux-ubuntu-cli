5df8b4466b8bc303148eb0f5921331b13d701554
abhiram79 <abhiramv670@gmail.com>
2024-12-20 10:40:40 +0530

    Fix  syntax error

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ Y="$(printf '\033[1;33m')"
 W="$(printf '\033[1;37m')"
 C="$(printf '\033[1;36m')"
 
-banner(){
+
 clear
 printf ${Y}"UBUNTU CLI - TERMUX"${W}
 printf ${Y}"github.com/abhiram79"${W}
-}
+
 CHROOT=$PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu
-install_ubuntu(){
 echo
 if [[ -d "$PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu" ]]; then
 echo ${G}"Existing Ubuntu installation found, Resetting it..."${W}
@@ -24,8 +23,8 @@ echo
 pkg install proot-distro
 proot-distro install ubuntu
 fi
-}
-adding_user(){
+
+
 echo ${G}"Setting User."${W}
 cat > $CHROOT/root/.bashrc <<- EOF
 apt-get install sudo wget -y
@@ -41,9 +40,9 @@ proot-distro login ubuntu
 echo "proot-distro login --user ubuntu ubuntu" >> $PREFIX/bin/ubuntu
 chmod +x $PREFIX/bin/ubuntu
 rm $CHROOT/root/.bashrc
-}
-final_banner(){
-banner
+clear
+
+
 echo
 echo ${G}"Installion completed"
 echo
@@ -52,8 +51,4 @@ echo
 echo "ubuntu  -  default ubuntu password"
 echo
 rm -rf ~/install.sh
-}
-banner
-install_ubuntu
-adding_user
-final_banner
+
